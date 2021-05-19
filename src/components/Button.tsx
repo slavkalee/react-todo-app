506195722297e4a5ebf8eac1c1ef6dc1ab5fbe6f
Slava Lee <sl_lee@mail.ru>
2021-05-19 17:26:14 +0500

    fix some things

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ const Button: React.FC<ButtonProps> = memo(({ openModal }) => {
   return (
     <div className="circle-btn" data-tooltip="Add todo">
       <button className="btn-add" onClick={openModal}>
-        +
+        <span className="btn-add__text">+</span>
       </button>
     </div>
   );
